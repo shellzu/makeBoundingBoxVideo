66e1ef19dbfc3fdac50cc914df75bc6751603d2a
unknown <kaizuxs@hba.co.jp>
2023-08-03 13:44:27 +0900

    [clean]persons取得処理の関数化

--- lambda_function.py
+++ lambda_function.py
@@ -37,17 +37,34 @@ def loading_video(file_name):
     frame = np.array(frame)
     return video, video_fps, frame
 
+# 動画読み込み関数呼び出し
 video, video_fps, frame = loading_video(file_name)
 
 # 何してるか不明(加工前後で差分なし)
 _, height, width, _ = frame.shape
 
-# JSONデータ読込
-with open('{}.json'.format(file_name), 'r') as f:
-    json_data = json.load(f)
+def get_persons(file_name):
+    """persons取得関数
 
-####### json_data['Labels']に変更 #######
-persons = json_data['Persons']
+    Parameters:
+    ----------
+    file_name : str
+        Rekognitionによって生成されるJSONファイル名
+    
+    Returns:
+    ----------
+    persons : dict
+        persons(人を認識した場合に出力される階層)
+    """
+    # JSONデータ読込
+    with open('{}.json'.format(file_name), 'r') as f:
+        json_data = json.load(f)
+
+    ####### json_data['Labels']に変更 #######
+    return json_data['Persons']
+
+persons = get_persons(file_name)
+
 target = []
 
 for i, person in enumerate(persons):
--- test_lambda_function.py
+++ test_lambda_function.py
@@ -2,13 +2,19 @@ import unittest
 import lambda_function
 
 class TestFunc(unittest.TestCase):
-    def test_func(self):
+    def test_loading_video(self):
         file_name = 'IMG_3656_lambda'
         expected1, expected2, expected3 = 'VideoCapture', 25.0, 'ndarray'
         actual1, actual2, actual3 = lambda_function.loading_video(file_name)
         self.assertEqual(expected1, type(actual1).__name__) # 返却値1はクラス名で比較
         self.assertEqual(expected2, actual2)
         self.assertEqual(expected3, type(actual3).__name__) # 返却値3はクラス名で比較
+
+    def test_get_persons(self):
+        file_name = 'IMG_3656_lambda'
+        expected = {'Timestamp': 0, 'Person': {'Index': 0, 'BoundingBox': {'Width': 0.17656250298023224, 'Height': 0.7138888835906982, 'Left': 0.16562500596046448, 'Top': 0.11388888955116272}}}
+        actual = lambda_function.get_persons(file_name)
+        self.assertEqual(expected, actual[0])
 
 if __name__ == '__main__':
     unittest.main()
